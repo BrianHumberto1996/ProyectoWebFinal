d6f9d5a7e1259aa82ce19bf654b8b12d796f0326
Jonathan <jonymadara@live.com.mx>
2017-12-06 20:28:50 -0600

    Inicio de letras en home

--- vista/Home.php
+++ vista/Home.php
@@ -16,32 +16,32 @@
 	<title>Document</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/font-awesome.css">
-    
+
 </head>
 <body>
-    
+
 <nav class="navbar navbar-inverse bg-primary">
-    
+
 <form class="form-inline my-2 my-lg-0">
 <button type="button" class="btn btn-primary" style="font-size: 20px; font-weight: bold">Sistema de tutorias</button>
 <button type="button" class="btn btn-primary">inicio</button>
-<button type="button" class="btn btn-primary">Grupos</button>    
+<button type="button" class="btn btn-primary">Grupos</button>
 
 </form>
 <span style="font-family: Arial; margin-left: 700px; font-weight: bold" ><?php echo $_SESSION["usuario"]->nombre; ?> </span>
 <button type="button" class="btn btn-outline-warning" name="out">cerrar secion</button>
 
 </nav>
-    
-    
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 <footer class="bg-primary" style="position: absolute; bottom: 0; width: 100%; text-algin: center">
     <span>Francisco Avalos Murillo</span>
     <br>
     <span>Humberto Diaz</span>
     <br>
     <span>Jonathan Camacho Lopez</span>
-    
-    
+
+
 </footer>
 
 	<script src="js/jquery-3.2.1.slim.min.js"></script>
